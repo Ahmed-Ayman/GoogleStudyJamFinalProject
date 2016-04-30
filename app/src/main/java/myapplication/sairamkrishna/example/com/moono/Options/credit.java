f851216623b1a49d925cd620bf070412e71116ba
Ahmed Ayman <Ahmed Ayman>
2016-04-30 16:54:45 +0200

    first commit

---
+++ app/src/main/java/myapplication/sairamkrishna/example/com/moono/Options/credit.java
@@ -0,0 +1,18 @@
+package myapplication.sairamkrishna.example.com.moono.Options;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import myapplication.sairamkrishna.example.com.moono.R;
+
+public class credit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit);
+
+
+
+    }
+}
